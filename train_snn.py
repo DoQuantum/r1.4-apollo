bf7229d9965cfe0c3df2a8447148143c557f7c05
BadjatiaH <badjatia.h@gmail.com>
2025-06-04 18:12:16 -0500

    Defined a basic SNN
    Defined an SNN model set up with a linear layer, then a LIF layer, followed by another linear and LIF layer. Set up the function to step it forward once in time and update the layers.

--- train_snn.py
+++ train_snn.py
@@ -9,3 +9,33 @@ train_snn.py
     4. Backpropagate via surrogate gradients
 – Save checkpoints and final trained weights
 """
+import torch
+import torch.nn as nn
+import snntorch as snn
+import snntorch.functional as SF
+import snntorch.surrogate as surrogate
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+import os
+
+class SNNModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(28*28, 100)
+        self.lif1 = snn.Leaky(beta=0.9, spike_grad=surrogate.fast_sigmoid())
+        self.fc2 = nn.Linear(100, 10)
+        self.lif2 = snn.Leaky(beta=0.9, spike_grad=surrogate.fast_sigmoid())
+
+    def forward(self, x, num_steps=10):
+        spk_out = None
+        for step in range(num_steps):
+            cur1 = self.fc1(x)
+            spk1, _ = self.lif1(cur1)
+            cur2 = self.fc2(spk1)
+            spk2, _ = self.lif2(cur2)
+
+            if spk_out is None:
+                spk_out = spk2
+            else:
+                spk_out += spk2
+        return spk_out / num_steps #Averaged output
